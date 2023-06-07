5b543bf6a8e3848389c0ef799b8343fbd39219be
Ajith2610 <135815075+Ajith2610@users.noreply.github.com>
2023-06-07 16:20:39 +0530

    Create seleniumwebdriver2.java

---
+++ seleniumwebdriver2.java
@@ -0,0 +1,29 @@
+package myseleniumdrive;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class seleniumwebdriver2 {
+
+	public static void main(String[] args) throws MalformedURLException {
+
+ChromeDriver driver=new ChromeDriver();
+		
+		WebDriver driver1=new ChromeDriver();
+		
+		SearchContext driver2=new ChromeDriver();
+		
+		Object driver3=new ChromeDriver();
+		
+		WebDriver driver4=new RemoteWebDriver(new URL(""),new DesiredCapabilities());
+		
+
+	}
+
+}
