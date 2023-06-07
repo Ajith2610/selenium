1d365a87d79da17cf36a360c48f1e17908880987
Ajith2610 <135815075+Ajith2610@users.noreply.github.com>
2023-06-07 16:14:13 +0530

    Create driver.java

---
+++ driver.java
@@ -0,0 +1,28 @@
+package myseleniumdrive;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class driver
+{
+
+	/*ChromeDriver
+	 *FirefoxDriver
+	 *EdgeDriver
+	 *SafariDriver
+	 *OperaminiDriver etc...
+	 */
+	
+	public static void main(String[] args)
+	{
+        ChromeDriver driver=new ChromeDriver();
+		
+		//FirefoxDriver driver1=new FirefoxDriver();
+		
+		//SafariDriver driver2=new SafariDriver();
+		
+		//EdgeDriver driver3=new EdgeDriver();
+		
+
+	}
+
+}
